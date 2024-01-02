524a4a3a1d1243918801769c36a262168c0916d2
SACCA LAFIA Om Nheath <saccalafiaomnheath@gmail.com>
2024-01-02 09:37:29 +0100

    COMPOSITION

---
+++ part2/script1.js
@@ -0,0 +1,6 @@
+let nom=String(prompt("entrez votre nom"))
+let prenom=String(prompt("entrez votre prenom"))
+let année=Number(prompt("Quel est votre année de naissance"))
+function age(année){
+
+    let age = nouvelle.Date (diff);
---
+++ part2/script3.js
@@ -0,0 +1,11 @@
+exercice3
+let word = String(prompt("entrez un mot"))
+function palindrome(word){
+if (word === word.split('').reverse().join('')){
+    return word + "is a palindrome"
+}
+else{
+    return word + "is not a palindrome"}
+ }
+ console.log(madam)
+l
